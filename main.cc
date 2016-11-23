7586be4c4b9f48983b1fc195fa73eb9dcaa9e4aa
Collimator <pawelwlastowski@gmail.com>
2016-11-23 14:28:45 +0100

    Fruit and banana inheritance

--- main.cc
+++ main.cc
@@ -4,101 +4,74 @@
 #include <array>
 #include <ctime>
 
-class Fraction
+class Fruit
 {
 private:
-int m_numerator = 0;
-int m_denumerator = 1;
+	std::string m_name;
+	std::string m_color;
 
 public:
-
-	Fraction(int numerator = 0 ,int denumerator = 1):m_numerator(numerator),m_denumerator(denumerator)
+	Fruit(std::string name,std::string color):m_name(name),m_color(color)
 	{
-	reduce();
-	}
-
-void	print()
-	{
-	std::cout << m_numerator << "/" << m_denumerator << std::endl;
-	}
-
-	friend Fraction operator*(const Fraction&, const Fraction&);
-	friend Fraction operator*(const Fraction&, int);
-	friend Fraction operator*(int, const Fraction&);
-
-	friend std::ostream& operator<<(std::ostream&,const Fraction&);
-	friend std::istream& operator>>(std::istream&, Fraction&);
-
-static int gcd(int a, int b) {
-    return b == 0 ? a : gcd(b, a % b);
-}
-
-void	reduce()
-	{
-	int gcd_x=gcd(m_numerator,m_denumerator);
-	/*if(gcd_x!=0)
-		{
-		m_numerator/=gcd_x;
-		m_denumerator/=gcd_x;
-		}
-	}
-	*/
 	
-		int gcd = Fraction::gcd(m_numerator, m_denumerator);
-		m_numerator /= gcd;
-		m_denumerator /= gcd;
+	}
+
+	std::string getName() const
+	{
+	return m_name;
 	}
 	
+	std::string getColor() const
+	{
+	return m_color;
+	}
 
 };
 
-	Fraction operator*(const Fraction &f1, const Fraction &f2)
-	{	
-	return Fraction(f1.m_numerator*f2.m_numerator,f1.m_denumerator*f2.m_denumerator);
-	}
-	
-	Fraction operator*(const Fraction& f1, int num)
+class Apple : public Fruit
+{
+private:
+	double m_fiber;
+
+public:
+	Apple(std::string name,std::string color,double fiber): Fruit(name, color), m_fiber(fiber)
 	{
-	return Fraction(f1.m_numerator*num,f1.m_denumerator);
-	}
-	
-	Fraction operator*(int num, const Fraction& f1)
-	{
-	return f1*num;
+
 	}
 
-	std::ostream& operator<<(std::ostream &out, const Fraction &f)
+friend std::ostream& operator<<(std::ostream &out, const Apple &apple)
 	{
-	out << f.m_numerator <<"/"<< f.m_denumerator;
+	std::cout << "Apple(" << apple.getName() << ", " << apple.getColor() << ", " << apple.m_fiber << ")\n" ;
 	return out;
 	}
 
-	std::istream& operator>>(std::istream& in, Fraction& f)
-	{
-	in >> f.m_numerator;
-	in >> f.m_denumerator;
 
-	return in; 
+};
+
+class Banana : public Fruit
+{
+public: 
+	Banana(std::string name,std::string color): Fruit(name, color)
+	{
 	}
 
+friend std::ostream& operator<<(std::ostream &out, const Banana &banana)
+	{
+	std::cout << "Apple(" << banana.getName() << ", " << banana.getColor() << ")\n" ;
+	return out;
+	}
+
+};
+
 int main()
-{
+{	
 
- 
-	Fraction f1;
-	std::cout << "Enter fraction 1: \n";
-	std::cin >> f1;
-	f1.print();
- 
-	Fraction f2;
-	std::cout << "Enter fraction 2: \n";
-	std::cin >> f2;
-	f2.print();
-	
-	Fraction f3=f1*f2; 
+const Apple a("Red delicius", "red", 4.2);
+std::cout << a;
 
-	std::cout << f1 << " * " << f2 << " is "<< f1 * f2 << '\n';
-//	std::cout << (f1*f2); 
-	return 0; 
+const Banana b("Caavendish", "yellow");
+std::cout << b;
+
+return 0; 
 }
 
