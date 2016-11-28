16ecf2239a46e963117ae61e1b68b97fea970d44
Collimator <pawelwlastowski@gmail.com>
2016-11-28 15:40:08 +0100

    StudentGrade - Array. Subscript operator overloaded

--- main.cc
+++ main.cc
@@ -1,85 +1,53 @@
 #include <iostream>
-#include <string>
 #include <cstdlib>
 #include <array>
 #include <ctime>
+#include <vector>
+#include <string>
 
-class Point
+struct StudentGrade
 {
-private:
-	int m_x = 0;
-	int m_y = 0;
-	int m_z = 0;
- 
-public:
-	Point(int x, int y, int z)
-		: m_x(x), m_y(y), m_z(z)
-	{
- 
-	}
- 
-	friend std::ostream& operator<<(std::ostream &out, const Point &p)
-	{
-		out << "Point(" << p.m_x << ", " << p.m_y << ", " << p.m_z << ")";
-		return out;
-	}
+std::string student_name;
+char grade;
 };
 
-class Shape
-{
-protected:
-virtual std::ostream& print(std::ostream &out) const = 0;
-
-friend std::ostream& operator<<(std::ostream &out, const Shape &shape)
-{
-return shape.print(out);
-}
-
-virtual ~Shape()
-{}
-
-};
-
-class Triangle: public Shape
+class GradeMap
 {
 private:
-Point m_p1,m_p2,m_p3;
+std::vector<StudentGrade> m_map;
 
 public:
-Triangle(Point p1,Point p2,Point p3):m_p1(p1),m_p2(p2),m_p3(p3)
-{}
+GradeMap(){}
 
-std::ostream& print(std::ostream &out) const override
+char& operator[]( const std::string &name)
 {
-return std::cout << "Triangle(" << m_p1 << ", " << m_p2 << ", " << m_p3 <<") \n";
+int index = 0;
+index = m_map.size();
+
+for(index = 0; index < m_map.size(); index++) 
+if(m_map.at(index).student_name==name)
+return m_map.at(index).grade;
+
+
+StudentGrade x {name, ' '};
+m_map.push_back(x);
+
+return m_map.back().grade;
 }
 
-};
 
-class Circle: public Shape
-{
-private:
-Point m_center;
-int m_radius;
-
-public:
-Circle(Point center,int radius):m_center(center),m_radius(radius)
-{}
-
-std::ostream& print(std::ostream &out) const override
-{
-return std::cout << "Circle(" << m_center << "radius " << m_radius << ") \n";
-}
 
 };
 
 int main()
 {
 
-Circle c(Point(1,2,3),7);
-std::cout << c;
 
-Triangle t(Point(1, 2, 3), Point(4, 5, 6), Point(7, 8, 9));
-std::cout << t;
+	GradeMap grades;
+	grades["Joe"] = 'A';
+	grades["Frank"] = 'B';
+	std::cout << "Joe has a grade of " << grades["Joe"] << '\n';
+	std::cout << "Frank has a grade of " << grades["Frank"] << '\n';
+
 return 0;
 }
