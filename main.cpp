f43c35987bacde0841401c6bb710835a898e0ed8
Collimator <pawelwlastowski@gmail.com>
2016-12-10 12:24:49 +0100

    Codility: CountSemiprimes - Sieve of Eratosthenes

--- main.cpp
+++ main.cpp
@@ -8,40 +8,95 @@
 #include <algorithm>
 #include <iterator>
 #include <ctype.h>
+#include <cmath>
 
-std::string heavyMetalUmlauts(std::string boringText){
+using namespace std;
 
-  std::string::iterator it = boringText.begin();
-  std::string MyText;
+vector<int> solution(int N, vector<int> &P, vector<int> &Q) {
 
-  for(; it!=boringText.end(); ++it){
-    switch(*it){
-      case 'A': MyText.append( "\u00c4" ); break; 
-      case 'a': MyText.append( "\u00e4" ); break; 
-      case 'O': MyText.append( "\u00d6" ); break; 
-      case 'o': MyText.append( "\u00f6" ); break; 
-      case 'E': MyText.append( "\u00cb" ); break; 
-      case 'e': MyText.append( "\u00eb" ); break; 
-      case 'U': MyText.append( "\u00dc" ); break; 
-      case 'u': MyText.append( "\u00fc" ); break; 
-      case 'I': MyText.append( "\u00cf" ); break; 
-      case 'i': MyText.append( "\u00ef" ); break; 
-      case 'Y': MyText.append( "\u0178" ); break; 
-      case 'y': MyText.append( "\u00ff" ); break; 
-      default: MyText.append(1, *it);  break;
-    }
+  int x = 0;
+  const int range_val = static_cast<int> (sqrt(N))+1;
+  vector<bool> prime( N+1, true );
+  vector<bool> semi_prime( N+1, false );
+  vector<int> semiprime_counter( static_cast<int>( P.size() ) );
+  vector<bool>::iterator it = prime.begin();
+  
+  prime[0] = false;  
+  prime[1] = false;
+  
+  for(int i = 2; i < range_val; i++){
+    if( prime[i] )
+      for (int j=i*i; j <= N; ++x, j=i*i + x*i ){
+        prime[j] = false;
+      }
+    x = 0;
   }
 
-  return MyText;
+
+  for(int i = 2; i < range_val; ++i){
+    if(!prime[i])
+      continue;
+    else  
+      for(int j = i; j * i <= N; ++j)
+        if(!prime[j])
+          continue;
+        else
+          semi_prime[i*j] = true;
+  }
+  
+  /*
+  std::cout << "prime \n";
+  for(int i =0 ; i <= N; ++i)
+    std::cout <<  prime[i] << " ";
+    std::cout << " \nsemi prime \n";
+  for(int i =0 ; i <= N; ++i)
+    std::cout << semi_prime[i] << " ";
+  */
+  
+  for(int i = 0; i < static_cast<int>(P.size()); ++i)
+    semiprime_counter[i] = count(semi_prime.begin() + P[i] , semi_prime.begin() + Q[i] +1, true); 
+  
+  return semiprime_counter;
 }
 
+int main(){
 
+  vector<int> P {1, 5, 2};
+  vector<int> Q {26, 10, 5};
 
-int main()
-{
+  vector<int> Z = solution(26, P, Q);
 
-  std::cout << heavyMetalUmlauts("Announcing the Macbook Air Guitar") << '\n';
+  for( auto it : Z)  
+    std::cout << it << " ";
+  std::cout << "\n";
+
 
   return 0;
 }
+/* Algorithm
 
+1. Find min(P) and max(Q)
+2. Calculate all semiprimes between those numbers ~ How?
+   2.1 Known factors
+   ~   All primes as a result is not multiplicable by any number other than themselves and 1 - Not rootable
+   ~   Semiprimes as a result can be multipllication only by prime numbers
+   ~   Semiprimes are NOT primes
+   ~   xAll multiplications of primes are semiprimes
+   2.2 Possible methods
+   ~   Loop through numbers, and if not prime, check if it is multiplicable by NOT prime from the beginning - If is, it is not semiprime
+   ~   xCalculate all primes
+   ~   xMultiply all primes with themlselves and iterate up to sqrt(n)
+   
+3. Go through all comparisons between arrays and count number of semipremises from table.
+
+   Methods usage
+   
+1. Set up a vector of bools for prime numbers. array<bool, sqrt(N)> 
+2. Set up a vector of bools for semiprimes array<bool, sqrt(N)> 
+3. Set up a vector<M> for return container
+4. Calculate primes with true
+5. Calculate semiprimes as in 2.2 (~3) with true
+6. Iterate through all P[i] and Q[i] and count true between numbers.
+7. Push counter value into return vector
+
+*/
